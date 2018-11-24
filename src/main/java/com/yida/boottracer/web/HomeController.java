c47ffc26f2a6b57a83baad045bfbc29cc6a1b6c1
fxwdl <fx__wdl@hotmail.com>
2018-11-24 17:15:37 +0800

    增加菜单显示

--- src/main/java/com/yida/boottracer/web/HomeController.java
+++ src/main/java/com/yida/boottracer/web/HomeController.java
@@ -1,5 +1,10 @@
 package com.yida.boottracer.web;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.UUID;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -11,9 +16,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.yida.boottracer.BoottracerApplicationTests;
+import com.yida.boottracer.domain.DictSystemFunction;
 import com.yida.boottracer.domain.test.Order;
 import com.yida.boottracer.domain.test.User;
 import com.yida.boottracer.service.UserService;
+import com.yida.enums.FunctionEnum;
 
 @Controller
 public class HomeController
@@ -43,6 +50,27 @@ public class HomeController
 		request.setAttribute("appName", appName);
 		request.setAttribute("order", o);
 
+		List<DictSystemFunction> menu = new ArrayList<DictSystemFunction>();
+
+		DictSystemFunction L1 = new DictSystemFunction(UUID.randomUUID().toString(), "L1", "一级菜单A", "L1",
+				FunctionEnum.Module.ordinal());
+		L1.setCssClass("fa fa-ambulance");
+		L1.getChildren().add(new DictSystemFunction(UUID.randomUUID().toString(), L1, "L1_1", "二级菜单A1", "L1|L1_1",
+				FunctionEnum.Menu.ordinal(), "M001", "#", "", null));
+		L1.getChildren().add(new DictSystemFunction(UUID.randomUUID().toString(), L1, "L1_2", "二级菜单A2", "L1|L1_2",
+				FunctionEnum.Menu.ordinal(), "M002", "#", "", null));
+		menu.add(L1);
+
+		DictSystemFunction L2 = new DictSystemFunction(UUID.randomUUID().toString(), "L2", "一级菜单B", "L2",
+				FunctionEnum.Module.ordinal());
+		L2.getChildren().add(new DictSystemFunction(UUID.randomUUID().toString(), L1, "L2_1", "二级菜单B1", "L2|L2_1",
+				FunctionEnum.Menu.ordinal(), "M001", "#", "", null));
+		L2.getChildren().add(new DictSystemFunction(UUID.randomUUID().toString(), L1, "L2_2", "二级菜单B2", "L2|L2_2",
+				FunctionEnum.Menu.ordinal(), "M002", "#", "", null));
+		L2.setCssClass("fa fa-calculator");
+		menu.add(L2);
+
+		request.setAttribute("menu", menu);
 		return "index";
 	}
 
---
+++ src/main/java/com/yida/enums/FunctionEnum.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.yida.enums;
+
+/**
+ * @author fx__w 功能类型枚举
+ */
+public enum FunctionEnum
+{
+	/**
+	 * 模块
+	 */
+	Module,
+
+	/**
+	 * 菜单
+	 */
+	Menu,
+
+	/**
+	 * 控制器
+	 */
+	Controller,
+
+	/**
+	 * Action
+	 */
+	Action
+}
